a51bfac932ae436b0122704448310dc11c6d171a
Kasish Agarwal <32645743+KasishAgarwal@users.noreply.github.com>
2022-10-13 14:55:11 +0530

    Create largest-area-triangle.cpp

---
+++ largest-area-triangle.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    double largestTriangleArea(vector<vector<int>>& points) {
+        double ans = 0.0;
+        
+        for(int i = 0; i < points.size()-2; i++) {
+            for(int j = i+1; j < points.size()-1; j++) {
+                for(int k = j+1; k < points.size(); k++) {
+                    double area = 0.5 * abs(points[i][0]*(points[j][1] - points[k][1]) + 
+                                points[j][0]*(points[k][1] - points[i][1]) + 
+                                points[k][0]*(points[i][1] - points[j][1]));
+                    ans = max(ans, area);
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
